dc50a2804c3f2978b2b7559a5f119ed86c680702
Le0wang06 <098leowang@gmail.com>
2025-08-26 22:08:12 -0700

    created main page

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   get "about", to: "about#index"
+
+  get "/", to: "main#index"
 end
